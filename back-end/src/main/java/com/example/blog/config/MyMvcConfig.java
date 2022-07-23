32c947f0d0a06b14ea0fc636a3f06388d626d8e0
Peng Ma <peng.ma@thoughtworks.com>
2022-07-23 13:14:21 +0800

    [feat] add HelloInterceptor & config MvcConfig

---
+++ back-end/src/main/java/com/example/blog/config/MyMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.blog.config;
+
+import com.example.blog.interceptor.HelloInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new HelloInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/",
+                        "/webjars/**",
+                        "/v3/**",
+                        "/swagger-resources/**",
+                        "/doc*"
+                );
+    }
+}
